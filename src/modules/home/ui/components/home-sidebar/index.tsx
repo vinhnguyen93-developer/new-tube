494ecda59b5962201cb4ae84d216a27ae0a3546d
VinhNguyen <vinh@cck.co.jp>
2025-03-11 23:04:13 +0700

    [Implement] Basic layout

---
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -0,0 +1,17 @@
+import { MainSection } from "./main-section";
+import { PersonalSection } from "./personal-section";
+
+import { Separator } from "@/components/ui/separator";
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+
+export const HomeSidebar = () => {
+  return (
+    <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+      <SidebarContent className="bg-background">
+        <MainSection />
+        <Separator />
+        <PersonalSection />
+      </SidebarContent>
+    </Sidebar>
+  );
+};
---
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Subscriptions",
+    url: "/feed/subscriptios",
+    icon: PlaySquareIcon,
+    auth: true,
+  },
+  {
+    title: "Trending",
+    url: "/feed/trending",
+    icon: FlameIcon,
+    auth: true,
+  },
+];
+
+export const MainSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // TODO: Change to look at current pathname
+                onClick={() => {}} // TODO: Do something on click
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
